33dd8e2b7aac2032185d24d3eeb95ff769957135
Omar Faleh <omar@morscad.com>
2024-03-12 08:15:11 -0400

    Revert "access data directly"
    This reverts commit 889ecb04490e63a7c1590a9f6c71c36f9b84c67a.

--- pages/livres/[bookName]/index.vue
+++ pages/livres/[bookName]/index.vue
@@ -40,7 +40,7 @@
       return  { chapters: bookResult, book: currentBook }
     })
 
-    const { chapters, book } = data
+    const { chapters, book } = data._rawValue
     bookCategory.value = book
     bookCover.value = chapters[0]
     bookChapters.value = chapters.slice(1)
