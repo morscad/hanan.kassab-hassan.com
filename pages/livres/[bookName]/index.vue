0f98ab996d84ed7229c19315e8fbdfe685314215
Omar Faleh <omar@morscad.com>
2024-03-13 09:23:46 -0400

    log articles

--- pages/livres/[bookName]/index.vue
+++ pages/livres/[bookName]/index.vue
@@ -28,6 +28,7 @@
   try {
     
     const { data, error} = await useAsyncData(`book-category-${bookName}`, () =>  $fetch(`${apiBasePath}/posts?categories=${categoryID}`));
+    console.log(data.value)
     const chapterList: WPArticle[] = data.value as WPArticle[] || []
     const bookResult = chapterList.toSorted((a: WPArticle, b: WPArticle) => { return new Date(a.date) >= new Date(b.date) ? 1 : -1 })
     bookCategory.value = categoryID
